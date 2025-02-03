5344e555fc04e68147502c2abade2de2bd3b3c7e
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-03 23:30:09 +0100

    footer

---
+++ react-dc-comics/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        background: "#000",
+        color: "#fff",
+        padding: "2rem",
+        textAlign: "center",
+      }}
+    >
+      <p>DC Comics &copy; 2024 - All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
