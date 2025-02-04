aa4392f9837b4ce02323919747a00419c86ceca0
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-04 18:43:31 +0100

    fix images

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,20 +11,19 @@ const Section = () => {
       }}
     >
       <div>
-        <img src="public/images/buy-comics-digital-comics"></img> DIGITAL COMICS
+        <img src="/images/buy-comics-digital-comics.png"></img> DIGITAL COMICS
       </div>
       <div>
-        <img src="public/images/buy-comics-merchandise"></img> DC MERCHANDISE
+        <img src="/images/buy-comics-merchandise.png"></img> DC MERCHANDISE
       </div>
       <div>
-        <img src="public/images/buy-comics-subscriptions"></img> SUBSCRIPTION
+        <img src="/images/buy-comics-subscriptions.png"></img> SUBSCRIPTION
       </div>
       <div>
-        <img src="public/images/buy-comics-shop-locator"></img> COMIC SHOP
-        LOCATOR
+        <img src="/images/buy-comics-shop-locator.png"></img> COMIC SHOP LOCATOR
       </div>
       <div>
-        <img src="public/images/buy-dc-power-visa"></img> DC POWER VISA
+        <img src="/images/buy-dc-power-visa.svg"></img> DC POWER VISA
       </div>
     </section>
   );
