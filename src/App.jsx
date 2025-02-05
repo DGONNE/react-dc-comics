19102f5cb94c14581fa8cbe4020e373beb9a2a8a
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-05 23:56:17 +0100

    creazione card manipolando array

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
 import Footer from "./components/Footer";
+import Card from "./components/Card";
 import "./index.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Card />
       <Navbar />
       <Section />
       <Main />
@@ -17,5 +19,20 @@ function App() {
     </div>
   );
 }
+
+import comics from "./data"; // Importiamo l'array
+
+const App = () => {
+  return (
+    <div className="container">
+      <h2>Current Series</h2>
+      <div className="card-container">
+        {comics.map((comic) => (
+          <Card key={comic.id} title={comic.title} image={comic.image} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
