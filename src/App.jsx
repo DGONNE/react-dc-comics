7915323bf02bcce202f6f380834047f37931b51c
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-06 17:44:50 +0100

    project def

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,15 @@ import Footer from "./components/Footer";
 import Card from "./components/Card";
 import "./index.css";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Card />
-      <Navbar />
-      <Section />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
-
-import comics from "./data"; // Importiamo l'array
-
 const App = () => {
   return (
-    <div className="container">
-      <h2>Current Series</h2>
-      <div className="card-container">
-        {comics.map((comic) => (
-          <Card key={comic.id} title={comic.title} image={comic.image} />
-        ))}
-      </div>
+    <div className="app-container">
+      <Header />
+      <Navbar />
+      <Main />
+      <Section />
+      <Card />
+      <Footer />
     </div>
   );
 };
